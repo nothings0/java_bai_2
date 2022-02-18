d86a22ee7380029b55965877919e5610c8c6bd5d
Nothingtosay <79694084+nothings0@users.noreply.github.com>
2022-02-18 14:04:21 +0700

    Create Bai2.java

---
+++ Bai2.java
@@ -0,0 +1,36 @@
+package javaapplication5;
+
+import java.util.Scanner;
+
+public class Bai4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        byte a, b = 0;
+        System.out.print("nhap a: ");
+        while (true) {
+            try {
+                a = Byte.parseByte(sc.nextLine());
+                break;
+            } catch (Exception ex) {
+                System.out.println("Vui long nhap byte !");
+                System.out.print("nhap a: ");
+            }
+        }
+        System.out.print("nhap b: ");
+        while (true) {
+            try {
+                b = Byte.parseByte(sc.nextLine());
+                break;
+            } catch (Exception ex) {
+                System.out.println("Vui long nhap byte !");
+                System.out.print("nhap b: ");
+            }
+        }
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+        System.out.println(Math.pow(a, b));  
+    }
+}
